2f79c4810b5896bd8a57cde69dd56f4e36e1241e
dengmengmian(麻凡) <my@dengmengmian.com>
2025-01-13 15:14:13 +0800

    feat:增加注释

--- pkg/ip/ipinfo.go
+++ pkg/ip/ipinfo.go
@@ -1,3 +1,8 @@
+// Copyright 2024 Innkeeper GoTribe <https://www.gotribe.cn>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://www.gotribe.cn
+
 package ip
 
 import (
